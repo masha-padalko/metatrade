7f7e997adbb96cabf0218e3f4c564edd41d67f12
masha-padalko <masha_padalko@yahoo.com>
2023-06-29 02:09:32 +0300

    slick slider adaptive

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,22 @@ $(document).ready(function () {
     slidesToShow: 1, // Number of slides to show initially
     slidesToScroll: 1,
     mobileFirst:true,
+    adaptiveHeight: true,
     responsive: [
-      // {
-      //   breakpoint: 500,
-      //   settings: {
-      //     centerPadding: '100px',
-      //     centerMode: true,
-      //   }
-      // },
+      {
+        breakpoint: 1024,
+        settings: {
+          centerPadding: '200px',
+          centerMode: true,
+          
+        }
+      },
       {
         breakpoint: 1500,
         settings: {
           centerPadding: '408px',
           centerMode: true,
+          dots: true,
         }
       },
     ]
