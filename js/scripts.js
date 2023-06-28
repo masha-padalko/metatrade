aec74025abce132488f6d77c55c3a1b0397253ce
masha-padalko <masha_padalko@yahoo.com>
2023-06-28 13:18:49 +0300

    description mobile block

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,34 @@
 $(document).ready(function () {
   $(".carousel").slick({
-    dots: false,
+    dots: true,
     arrows: true,
-    adaptiveHeight: true
+    adaptiveHeight: true,
+    centerMode: true,
+    responsive: [
+      {
+        breakpoint: 1500,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 320,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 });
 
